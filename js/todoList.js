c9735ff3547ec6577266a87465fc4894ffcac332
im-somnus <somnusanimus@gmail.com>
2023-03-20 22:51:01 +0100

    Fixed text appearing on the right with the remove button (X)

--- js/todoList.js
+++ js/todoList.js
@@ -2,12 +2,8 @@ const todoList = document.getElementById("todoList");
 
 function addItem() {
     const newTask = document.createElement("li");
-    //const addTaskButton = document.getElementById("addTaskButton");
     const newText = document.getElementById("taskInput").value;
     const newTextNode = document.createTextNode(newText);
-    //newItem.appendChild("<img src='./images/taskUp.png'>");
-    //newItem.appendChild('<img src="../images/taskDown.png"/>')
-
 
     document.getElementById("taskInput").value = ""; // reseteamos el texto de la barra
 
@@ -18,10 +14,6 @@ function addItem() {
         <img id="downButton" src="images/taskDown.png" />
         <img id="removeButton" src="images/removeTask.png" />`;
 
-
-    //todoList.appendChild(newTask);
-
-    newTask.appendChild(newTextNode);
 
   const todoList = document.getElementById("todoList");
   todoList.appendChild(newTask);
@@ -35,8 +27,10 @@ function addItem() {
   removeButton.addEventListener("click", removeTask);
 }
 
+// Adds the new task when button is clicked and adds it to the bottom of the list
 const addTaskButton = document.getElementById("addTaskButton");
 addTaskButton.onclick = addItem;
+
 
 // Seleccionar todos los botones "taskUp"
 const taskUpButtons = document.querySelectorAll('img[src="images/taskUp.png"]');
@@ -64,47 +58,3 @@ removeButton.forEach(function (button) {
         todoList.removeChild(currentTask);
     });
 });
-
-
-//rm.onclick = removeItem();
-
-//removeTaskImage.foreach(function(button) {
-//button.addEventListener("click", function() {
-//    todoList.removeChild();
-//    })
-//
-//});
-
-// taskInput.addEventListener("Keypress", function(e))
-// if(e.key === "Enter")
-
-/*
-
-Para crear elements/text:
-    createElement
-    createTextNode
-    
-Para añadir elementos:
-    appendChild
-    insertBefore ("fundamental hoy)
-
-Para eliminar elementos:
-    removeChild(fundamental mañana)
-    
-Para acceder a elementos:
-    getElementById
-    querySelectorAll
-    querySelector
-        firstElementChild
-        lastElementChild
-        previousElementChild
-        nextElementChild
-        children
-        
-Para manipular atributos:
-    setAttribute (attName,value)
-    getAttribute (attName)
-    removeAttribute (attName)
-    hasAttribute (attName)
-
-*/
